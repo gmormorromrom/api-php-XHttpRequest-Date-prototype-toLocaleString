2d5c1f4e530d4a827a61e8b22d6dc013dc5ec3db
r0mdhan <gmormorromrom@gmail.com>
2023-09-29 20:44:54 +0100

    multi array names
    add multi array in names

--- api/customers.php
+++ api/customers.php
@@ -4,7 +4,7 @@ header("HTTP/1.1 200 OK");
 if(isset($_GET)){
     $data=[];
     for($i=1;$i<101;$i++){
-        $data[]=["name"=>"name".$i];
+    $data[]=[["firstname"=>"firstname".$i],["lastname"=>"lastname".$i]];
     }
     $date = new DateTime("now");
     echo json_encode(["message"=>"All names", "date"=>$date,"names"=>$data]);
--- index.php
+++ index.php
@@ -12,20 +12,32 @@
     <div id="customersid">...</div>
     <script>
         function getCustomers() {
-            let urlCustomers = "/api/customers.php" 
+            let urlCustomers = "/projectcrud/api/customers.php"
             const xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    console.log(this.response); 
+                    console.log(this.response);
                     var objData = JSON.parse(this.responseText);
-                    document.getElementById("customersid").innerHTML ="";
+                    document.getElementById("customersid").innerHTML = "";
                     // for(let key in objData.date)
-                    document.getElementById("customersid").innerHTML += "Title : " +objData.message+'<br>';
-                    var date = new Date(Date.parse(objData.date.date)); 
-                    var dateFR = date.toLocaleString('fr-FR', { timeZone: 'Africa/Tunis' });
-                    document.getElementById("customersid").innerHTML += "Date and time(Fr) : " +dateFR+'<br>';
-                    for(let key in objData.names){
-                        document.getElementById("customersid").innerHTML += parseInt(key)+1+"-"+objData.names[key].name +'<br>';
+                    document.getElementById("customersid").innerHTML += "Title : " + objData.message + '<br>';
+                    var date = new Date(Date.parse(objData.date.date));
+                    var dateFR = date.toLocaleString('fr-FR', {
+                        timeZone: 'Africa/Tunis'
+                    });
+                    document.getElementById("customersid").innerHTML += "Date and time(Fr) : " + dateFR + '<br>';
+                    for (let key in objData.names) {
+                        // document.getElementById("customersid").innerHTML += parseInt(key)+1+"-"+objData.names[key].firstname +'<br>';
+                        var name = "";
+                        for (let k in objData.names[key]) {
+                            if (k == 0) {
+                                name = objData.names[key][k].firstname;
+                            } else if (k == 1) {
+                                name += " " + objData.names[key][k].lastname;
+                            }
+
+                        }
+                        document.getElementById("customersid").innerHTML += parseInt(key) + 1 + "-" + name+ '<br>';
                     }
                 }
             };

1ae0c857c1ef929bb33231b305932cd64416a59e
r0mdhan <gmormorromrom@gmail.com>
2023-09-29 20:22:07 +0100

    Update index.php fix URI
    fix URI

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <div id="customersid">...</div>
     <script>
         function getCustomers() {
-            let urlCustomers = "/projectcrud/api/customers.php" 
+            let urlCustomers = "/api/customers.php" 
             const xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
